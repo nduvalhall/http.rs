a583ed75437b475c1fb29624cf96c4b6f650d4e5
nduvalhall <nduvalhall@gmail.com>
2026-04-27 15:48:22 +0100

    removed the need for all routes returning a result, implemented IntoResponse for Result<T, E> so that if you do return result it works

---
+++ src/method.rs
@@ -0,0 +1,21 @@
+#[derive(PartialEq, Eq)]
+pub enum Method {
+    Get,
+    Post,
+    Put,
+    Delete,
+    Option,
+}
+
+impl Method {
+    pub fn from_str(method: &str) -> Method {
+        match method {
+            "GET" => Method::Get,
+            "POST" => Method::Post,
+            "PUT" => Method::Put,
+            "DELETE" => Method::Delete,
+            "OPTION" => Method::Option,
+            _ => panic!("Unknow method: {}", method),
+        }
+    }
+}
